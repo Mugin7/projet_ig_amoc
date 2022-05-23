9c7cf3302dd897867814cb5d5abf5e565105da9f
Marius Chamvoux <m.chamvoux@gmail.com>
2022-05-23 12:47:17 +0200

    Commit initial

---
+++ src/application/Main.java
@@ -0,0 +1,26 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+
+
+public class Main extends Application {
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("Intro.fxml"));
+	        Scene scene = new Scene(fxmlLoader.load(), 600, 400);
+	        primaryStage.setTitle("Le Pendu AMOC");
+	        primaryStage.setScene(scene);
+	        primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
